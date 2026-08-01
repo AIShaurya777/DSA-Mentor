db92ba260a32ab73056eeac5d52be80356daaf9b
AIShaurya777 <shaurya.sangwan777@gmail.com>
2026-08-01 22:14:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://dsa-mentor-pied.vercel.app",
+    "https://dsa-mentor-4mzi990f-dsa-mentor.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
